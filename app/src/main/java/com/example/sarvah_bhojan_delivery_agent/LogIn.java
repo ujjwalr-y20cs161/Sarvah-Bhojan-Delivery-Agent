baa95d5943ce0d01cccc608fad91f1596ad98393
Ujjwal <y20cs161@rvrjc.ac.in>
2023-05-22 15:54:42 +0530

    Working stage of the previous commits

--- app/src/main/java/com/example/sarvah_bhojan_delivery_agent/LogIn.java
+++ app/src/main/java/com/example/sarvah_bhojan_delivery_agent/LogIn.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.color.DynamicColors;
+
 public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        DynamicColors.applyToActivityIfAvailable(this);
     }
 }
