edc22a358d13bec93472dc58640034f982202353
Ujjwal <y20cs161@rvrjc.ac.in>
2023-05-22 13:56:38 +0530

    Splash Screen Theme is added and a New Empty Log In Activity

---
+++ app/src/main/java/com/example/sarvah_bhojan_delivery_agent/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.example.sarvah_bhojan_delivery_agent;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class SplashScreen extends AppCompatActivity {
+    TextView Message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        this.getSupportActionBar().hide();
+        Message = (TextView) findViewById(R.id.app_title);
+        Message.setText("Welcome to "+getText(R.string.app_name));
+    }
+}
