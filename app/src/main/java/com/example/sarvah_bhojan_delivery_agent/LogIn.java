6bedc4489f7169b2eac6e4601a54abf3cf81b5a8
Ujjwal <y20cs161@rvrjc.ac.in>
2023-05-22 18:18:49 +0530

    Onclick listeners added, Firebase Dependencies added, Firebase imported in LogIn.java

--- app/src/main/java/com/example/sarvah_bhojan_delivery_agent/LogIn.java
+++ app/src/main/java/com/example/sarvah_bhojan_delivery_agent/LogIn.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.color.DynamicColors;
 import com.google.android.material.textfield.TextInputEditText;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogIn extends AppCompatActivity {
 
@@ -19,14 +24,34 @@ public class LogIn extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
         DynamicColors.applyToActivityIfAvailable(this);
 
+        FirebaseApp.initializeApp(this);
+
         signIn = (Button) findViewById(R.id.SignIn);
         signUp = (Button) findViewById(R.id.SignUp);
         forgotPassword = (Button) findViewById(R.id.pswdfrgt);
+
+        email = (TextInputEditText) findViewById(R.id.emailfield);
+        password = (TextInputEditText)findViewById(R.id.passwordfield);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                Toast.makeText(LogIn.this, email.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LogIn.this, password.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LogIn.this, "OTP is sent to your mail!", Toast.LENGTH_SHORT).show();
             }
         });
     }
