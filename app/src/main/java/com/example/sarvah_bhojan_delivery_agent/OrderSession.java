8fa9e6087804df51f2df074a822f30f1626e2e22
Ujjwal <y20cs161@rvrjc.ac.in>
2023-06-11 15:25:50 +0530

    Added Order Session

---
+++ app/src/main/java/com/example/sarvah_bhojan_delivery_agent/OrderSession.java
@@ -0,0 +1,35 @@
+package com.example.sarvah_bhojan_delivery_agent;
+
+
+public class OrderSession {
+    private static OrderSession instance;
+    private Order order;
+
+    private OrderSession() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static synchronized OrderSession getInstance() {
+        if (instance == null) {
+            instance = new OrderSession();
+        }
+        return instance;
+    }
+
+    public void setUser(Order order) {
+        this.order = order;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public boolean isOrderAccepted() {
+        return order != null;
+    }
+
+    public void clearSession() {
+        order = null;
+    }
+}
+
